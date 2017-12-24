b57252df90924742012350c05fa31657da745510
RaviGarbujaPun <cbpmgr@gmail.com>
2017-12-24 14:23:00 +0545

    Add model class

---
+++ app/src/main/java/np/com/ravi/assetsdemo/Employee.java
@@ -0,0 +1,16 @@
+package np.com.ravi.assetsdemo;
+
+/**
+ * Created by ravi on 12/24/17.
+ */
+
+public class Employee {
+    /* POJO Class*/
+    String name;
+    Integer age;
+    String position;
+    Integer salary;
+
+    public Employee() {
+    }
+}
